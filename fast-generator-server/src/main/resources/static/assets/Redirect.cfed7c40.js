4807bd374441d94a97d57af280e232033fcd6da4
阿沐 <babamu@126.com>
2022-05-05 21:07:17 +0800

    整合前端

--- fast-generator-server/src/main/resources/static/assets/Redirect.cfed7c40.js
+++
@@ -1,1 +0,0 @@
-import{a as o,b5 as s,b7 as n,ao as c}from"./index.d41c2b9a.js";const d=o({created(){const{params:e,query:a}=s(),{path:r}=e;n().replace({path:"/"+r,query:a}).catch(t=>{console.warn(t)})},render(){return c("div")}});export{d as default};
