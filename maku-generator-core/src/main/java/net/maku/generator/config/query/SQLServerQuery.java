6fcefee0540b8e3567187e73115e2f6bfeda127b
阿沐 <babamu@126.com>
2022-11-24 00:23:36 +0800

    修复SQLServer报错

--- maku-generator-core/src/main/java/net/maku/generator/config/query/SQLServerQuery.java
+++ maku-generator-core/src/main/java/net/maku/generator/config/query/SQLServerQuery.java
@@ -18,71 +18,64 @@ public class SQLServerQuery implements AbstractQuery {
     @Override
     public String tableSql(String tableName) {
         StringBuilder sql = new StringBuilder();
-        sql.append("SELECT DISTINCT d.name as table_name, f.value as table_comment FROM syscolumns a " +
-                "LEFT JOIN systypes b ON a.xusertype= b.xusertype " +
-                "INNER JOIN sysobjects d ON a.id= d.id  AND d.xtype= 'U' AND d.name<> 'dtproperties' " +
-                "LEFT JOIN syscomments e ON a.cdefault= e.id " +
-                "LEFT JOIN sys.extended_properties g ON a.id= g.major_id AND a.colid= g.minor_id " +
-                "LEFT JOIN sys.extended_properties f ON d.id= f.major_id AND f.minor_id= 0 ");
-        sql.append("where 1=1 ");
+        sql.append("select cast(so.name as varchar(500)) as TABLE_NAME, cast(sep.value as varchar(500)) as COMMENTS from sysobjects so ");
+        sql.append("left JOIN sys.extended_properties sep on sep.major_id=so.id and sep.minor_id=0 where (xtype='U' or xtype='V') ");
+
         // 表名查询
         if (StrUtil.isNotBlank(tableName)) {
-            sql.append("and d.name = '").append(tableName).append("' ");
+            sql.append("and cast(so.name as varchar(500)) = '").append(tableName).append("' ");
         }
-        sql.append("order by d.name asc");
+        sql.append(" order by cast(so.name as varchar(500))");
+
         return sql.toString();
     }
 
     @Override
+    public String tableFieldsSql() {
+        return "SELECT  cast(a.name AS VARCHAR(500)) AS TABLE_NAME,cast(b.name AS VARCHAR(500)) AS COLUMN_NAME, "
+                + "cast(c.VALUE AS NVARCHAR(500)) AS COMMENTS,cast(sys.types.name AS VARCHAR (500)) AS DATA_TYPE,"
+                + "(SELECT CASE count(1) WHEN 1 then 'PRI' ELSE '' END"
+                + " FROM syscolumns,sysobjects,sysindexes,sysindexkeys,systypes "
+                + " WHERE syscolumns.xusertype = systypes.xusertype AND syscolumns.id = object_id (a.name) AND sysobjects.xtype = 'PK'"
+                + " AND sysobjects.parent_obj = syscolumns.id  AND sysindexes.id = syscolumns.id "
+                + " AND sysobjects.name = sysindexes.name AND sysindexkeys.id = syscolumns.id "
+                + " AND sysindexkeys.indid = sysindexes.indid "
+                + " AND syscolumns.colid = sysindexkeys.colid AND syscolumns.name = b.name) as 'KEY',"
+                + "  b.is_identity isIdentity "
+                + " FROM ( select name,object_id from sys.tables UNION all select name,object_id from sys.views ) a "
+                + " INNER JOIN sys.columns b ON b.object_id = a.object_id "
+                + " LEFT JOIN sys.types ON b.user_type_id = sys.types.user_type_id   "
+                + " LEFT JOIN sys.extended_properties c ON c.major_id = b.object_id AND c.minor_id = b.column_id "
+                + " WHERE a.name = '%s' and sys.types.name !='sysname' ";
+    }
+
+    @Override
     public String tableName() {
-        return "table_name";
+        return "TABLE_NAME";
     }
 
     @Override
     public String tableComment() {
-        return "table_comment";
-    }
-
-    @Override
-    public String tableFieldsSql() {
-        return " Select SCOL.NAME as column_name, STYPE.NAME AS data_type , (case when(\n" +
-                "  SELECT COUNT (*) AS Is_PK\n" +
-                " FROM syscolumns\n" +
-                " JOIN sysindexkeys ON syscolumns.id = sysindexkeys.id AND syscolumns.colid = sysindexkeys.colid\n" +
-                " JOIN sysindexes ON syscolumns.id = sysindexes.id AND sysindexkeys.indid = sysindexes.indid\n" +
-                " JOIN sysobjects ON sysindexes.name = sysobjects.name AND sysobjects.xtype = 'PK'\n" +
-                " WHERE syscolumns.name = SCOL.NAME AND syscolumns.id = SCOL.ID\n" +
-                " ) > 0 then 'PRI' else '' end) column_key,    \n" +
-                "\t(SELECT SYS.EXTENDED_PROPERTIES.VALUE FROM SYSCOLUMNS  \n" +
-                "\tINNER JOIN SYS.EXTENDED_PROPERTIES ON SYSCOLUMNS.ID = SYS.EXTENDED_PROPERTIES.MAJOR_ID   \n" +
-                "\tAND SYSCOLUMNS.COLID = SYS.EXTENDED_PROPERTIES.MINOR_ID \n" +
-                "\tINNER JOIN SYSOBJECTS ON SYSCOLUMNS.ID = SYSOBJECTS.ID  \n" +
-                "\tWHERE SYSOBJECTS.NAME = SO.NAME AND SYSCOLUMNS.NAME = SCOL.NAME) AS column_comment  \n" +
-                "\tfrom SYSCOLUMNS AS SCOL\n" +
-                "\tLEFT JOIN SYSOBJECTS SO ON SO.ID=SCOL.ID \n" +
-                "\tLEFT JOIN SYSTYPES AS STYPE ON STYPE.xtype=SCOL.xtype\n" +
-                "\tWhere \n" +
-                "\tSCOL.ID=OBJECT_ID('%s')\n" +
-                "\tAND STYPE.NAME<>'SYSNAME'";
+        return "COMMENTS";
     }
 
     @Override
     public String fieldName() {
-        return "column_name";
+        return "COLUMN_NAME";
     }
 
     @Override
     public String fieldType() {
-        return "data_type";
+        return "DATA_TYPE";
     }
 
     @Override
     public String fieldComment() {
-        return "column_comment";
+        return "COMMENTS";
     }
 
     @Override
     public String fieldKey() {
-        return "column_key";
+        return "KEY";
     }
 }
