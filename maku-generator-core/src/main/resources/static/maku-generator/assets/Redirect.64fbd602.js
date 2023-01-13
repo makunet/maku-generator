d611e34d144febd495475b08b57c7b6b34a04e5e
阿沐 <babamu@126.com>
2023-01-13 14:08:09 +0800

    更新打包文件

---
+++ maku-generator-core/src/main/resources/static/maku-generator/assets/Redirect.64fbd602.js
@@ -0,0 +1,1 @@
+import{d as o,x as s,D as n,G as c}from"./index.992116f0.js";const d=o({created(){const{params:e,query:r}=s(),{path:t}=e;n().replace({path:"/"+t,query:r}).catch(a=>{console.warn(a)})},render(){return c("div")}});export{d as default};
