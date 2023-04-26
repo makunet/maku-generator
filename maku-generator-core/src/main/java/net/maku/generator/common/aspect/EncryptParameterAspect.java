6adbd54c4bb83fa612b9958120299d08f05dd94f
likh27596 <likh27596@hundsun.com>
2023-04-26 10:56:44 +0800

    【update】AES加解密相关代码优化

--- maku-generator-core/src/main/java/net/maku/generator/common/aspect/EncryptParameterAspect.java
+++ maku-generator-core/src/main/java/net/maku/generator/common/aspect/EncryptParameterAspect.java
@@ -2,6 +2,7 @@ package net.maku.generator.common.aspect;
 
 import lombok.extern.slf4j.Slf4j;
 import net.maku.generator.common.annotation.EncryptParameter;
+import net.maku.generator.common.page.PageResult;
 import net.maku.generator.common.utils.EncryptUtils;
 import net.maku.generator.common.utils.Result;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -25,20 +26,25 @@ import java.util.Objects;
 @Component
 public class EncryptParameterAspect {
 
+    /**
+     * 切面方法：page、list、get、save、update、tableList
+     *
+     * @param proceedingJoinPoint
+     * @return
+     * @throws Throwable
+     */
     @Around("execution(* net.maku.generator.controller.DataSourceController.*(..))")
     public Object doProcess(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
 
         // 处理请求入参
         List<Object> methodArgs = this.getMethodArgs(proceedingJoinPoint);
         for (Object item : methodArgs) {
-            if (item.getClass().getPackage().getName().startsWith("net.maku.generator.entity")) {
-                handleItem(item, true);
-            }
+            handleItem(item, true);
         }
         Object result = proceedingJoinPoint.proceed();
 
         // 处理返回值
-        handleObject(result, false);
+        handleObject(result);
         return result;
     }
 
@@ -48,7 +54,7 @@ public class EncryptParameterAspect {
     private List<Object> getMethodArgs(ProceedingJoinPoint proceedingJoinPoint) {
         List<Object> methodArgs = new ArrayList<>();
         for (Object arg : proceedingJoinPoint.getArgs()) {
-            if (null != arg) {
+            if (Objects.nonNull(arg)) {
                 methodArgs.add(arg);
             }
         }
@@ -61,27 +67,21 @@ public class EncryptParameterAspect {
      * @param object
      * @throws Exception
      */
-    private void handleObject(Object object, boolean isDecrypt) throws Exception {
-
-        if (object instanceof Result) {
-            Object item = ((Result<?>) object).getData();
-            if (item.getClass().getPackage().getName().startsWith("net.maku.generator.entity")) {
-                handleItem(item, isDecrypt);
-            }
-            if (item instanceof List){
-                ((List<?>) item).forEach(f -> {
-                    if (f.getClass().getPackage().getName().startsWith("net.maku.generator.entity")) {
-                        try {
-                            handleItem(f, isDecrypt);
-                        } catch (Exception e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                });
-            }
-
+    private void handleObject(Object object) throws Exception {
+        // 仅处理类型是Result的返回对象
+        if (!(object instanceof Result) || Objects.isNull(((Result<?>) object).getData())) {
+            return;
         }
 
+        Object data = ((Result<?>) object).getData();
+        if (data instanceof List || data instanceof PageResult) {
+            List itemList = data instanceof List ? (List) data : ((PageResult<?>) data).getList();
+            itemList.forEach(f ->
+                    handleItem(f, false)
+            );
+        } else {
+            handleItem(data, false);
+        }
     }
 
     /**
@@ -90,20 +90,31 @@ public class EncryptParameterAspect {
      * @param item      需要加解密的对象
      * @param isDecrypt true：解密，false：加密
      */
-    private void handleItem(Object item, boolean isDecrypt) throws Exception {
-        Field[] fields = item.getClass().getDeclaredFields();
+    private void handleItem(Object item, boolean isDecrypt) {
+
+        // 只处理在entity包下面的对象
+        if (Objects.isNull(item.getClass().getPackage()) || !item.getClass().getPackage().getName().startsWith("net.maku.generator.entity")) {
+            return;
+        }
+
         // 遍历所有字段
+        Field[] fields = item.getClass().getDeclaredFields();
         for (Field field : fields) {
             // 若该字段被EncryptParameter注解,则进行解密/加密
             Class<?> fieldType = field.getType();
             if (fieldType == String.class && Objects.nonNull(AnnotationUtils.findAnnotation(field, EncryptParameter.class))) {
                 // 设置private类型允许访问
                 field.setAccessible(Boolean.TRUE);
-                field.set(item, isDecrypt ? EncryptUtils.decrypt((String) field.get(item)) : EncryptUtils.encrypt((String) field.get(item)));
+                try {
+                    String newFieldValue = isDecrypt ? EncryptUtils.decrypt((String) field.get(item)) : EncryptUtils.encrypt((String) field.get(item));
+                    field.set(item, newFieldValue);
+                } catch (Exception e) {
+                    log.error(e.getMessage(), e);
+                    throw new RuntimeException(e);
+                }
                 field.setAccessible(Boolean.FALSE);
             }
         }
     }
-
 
 }
