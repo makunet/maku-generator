5fbe693891627e7789564888c1d1c79dc80bef8d
阿沐 <babamu@126.com>
2023-04-28 14:03:32 +0800

    前端资源

--- maku-generator-core/src/main/resources/static/maku-generator/assets/baseClass.fe522f5c.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./index.cbafa6c9.js";const t=s=>e.get("/gen/baseclass/"+s),n=()=>e.get("/gen/baseclass/list"),i=s=>s.id?e.put("/gen/baseclass",s):e.post("/gen/baseclass",s);export{t as a,i as b,n as u};
