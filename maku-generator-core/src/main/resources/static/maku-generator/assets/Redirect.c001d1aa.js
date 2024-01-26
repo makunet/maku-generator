37ede2bf05cae6127b7d8408d6aec802727dbeb0
阿沐 <babamu@126.com>
2024-01-26 18:45:53 +0800

    更新前端打包文件

--- maku-generator-core/src/main/resources/static/maku-generator/assets/Redirect.c001d1aa.js
+++
@@ -1,1 +0,0 @@
-import{d as o,z as s,H as n,I as c}from"./index.7fff850b.js";const d=o({created(){const{params:e,query:r}=s(),{path:t}=e;n().replace({path:"/"+t,query:r}).catch(a=>{console.warn(a)})},render(){return c("div")}});export{d as default};
---
+++ maku-generator-core/src/main/resources/static/maku-generator/assets/baseClass-1c50f599.js
@@ -0,0 +1,1 @@
+import{s as e}from"./index-36840c2b.js";const t=s=>e.get("/gen/baseclass/"+s),n=()=>e.get("/gen/baseclass/list"),i=s=>s.id?e.put("/gen/baseclass",s):e.post("/gen/baseclass",s);export{t as a,i as b,n as u};
