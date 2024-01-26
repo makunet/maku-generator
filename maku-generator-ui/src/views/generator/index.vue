6e8d439556c9f3b8b32029c079509ba90dd77145
阿沐 <babamu@126.com>
2024-01-26 18:20:24 +0800

    格式化

--- maku-generator-ui/src/views/generator/index.vue
+++ maku-generator-ui/src/views/generator/index.vue
@@ -1,74 +1,73 @@
 <template>
-  <el-card>
-    <el-form :inline="true" :model="state.queryForm" @keyup.enter="getDataList()">
-      <el-form-item>
-        <el-input v-model="state.queryForm.tableName" placeholder="表名"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button @click="getDataList()">查询</el-button>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="importHandle()">导入</el-button>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="success" @click="downloadBatchHandle()">生成代码</el-button>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="danger" @click="deleteBatchHandle()">删除</el-button>
-      </el-form-item>
-    </el-form>
-    <el-table v-loading="state.dataListLoading" :data="state.dataList" border style="width: 100%"
-              @selection-change="selectionChangeHandle">
-      <el-table-column type="selection" header-align="center" align="center" width="50"></el-table-column>
-      <el-table-column prop="tableName" label="表名" header-align="center" align="center"></el-table-column>
-      <el-table-column prop="tableComment" label="表说明" header-align="center" align="center"></el-table-column>
-      <el-table-column prop="className" label="类名" header-align="center" align="center"></el-table-column>
-      <el-table-column label="操作" fixed="right" header-align="center" align="center" width="300">
-        <template #default="scope">
-          <el-button type="primary" link @click="editHandle(scope.row.id)">编辑</el-button>
-          <el-button type="primary" link @click="previewHandle(scope.row.id)">预览</el-button>
-          <el-button type="primary" link @click="generatorHandle(scope.row.id)">生成代码</el-button>
-          <el-button type="primary" link @click="deleteBatchHandle(scope.row.id)">删除</el-button>
-          <el-button type="primary" link @click="syncHandle(scope.row)">同步</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-    <el-pagination
-        :current-page="state.page"
-        :page-sizes="state.pageSizes"
-        :page-size="state.limit"
-        :total="state.total"
-        layout="total, sizes, prev, pager, next, jumper"
-        @size-change="sizeChangeHandle"
-        @current-change="currentChangeHandle"
-    >
-    </el-pagination>
+	<el-card>
+		<el-form :inline="true" :model="state.queryForm" @keyup.enter="getDataList()">
+			<el-form-item>
+				<el-input v-model="state.queryForm.tableName" placeholder="表名"></el-input>
+			</el-form-item>
+			<el-form-item>
+				<el-button @click="getDataList()">查询</el-button>
+			</el-form-item>
+			<el-form-item>
+				<el-button type="primary" @click="importHandle()">导入</el-button>
+			</el-form-item>
+			<el-form-item>
+				<el-button type="success" @click="downloadBatchHandle()">生成代码</el-button>
+			</el-form-item>
+			<el-form-item>
+				<el-button type="danger" @click="deleteBatchHandle()">删除</el-button>
+			</el-form-item>
+		</el-form>
+		<el-table v-loading="state.dataListLoading" :data="state.dataList" border style="width: 100%" @selection-change="selectionChangeHandle">
+			<el-table-column type="selection" header-align="center" align="center" width="50"></el-table-column>
+			<el-table-column prop="tableName" label="表名" header-align="center" align="center"></el-table-column>
+			<el-table-column prop="tableComment" label="表说明" header-align="center" align="center"></el-table-column>
+			<el-table-column prop="className" label="类名" header-align="center" align="center"></el-table-column>
+			<el-table-column label="操作" fixed="right" header-align="center" align="center" width="300">
+				<template #default="scope">
+					<el-button type="primary" link @click="editHandle(scope.row.id)">编辑</el-button>
+					<el-button type="primary" link @click="previewHandle(scope.row.id)">预览</el-button>
+					<el-button type="primary" link @click="generatorHandle(scope.row.id)">生成代码</el-button>
+					<el-button type="primary" link @click="deleteBatchHandle(scope.row.id)">删除</el-button>
+					<el-button type="primary" link @click="syncHandle(scope.row)">同步</el-button>
+				</template>
+			</el-table-column>
+		</el-table>
+		<el-pagination
+			:current-page="state.page"
+			:page-sizes="state.pageSizes"
+			:page-size="state.limit"
+			:total="state.total"
+			layout="total, sizes, prev, pager, next, jumper"
+			@size-change="sizeChangeHandle"
+			@current-change="currentChangeHandle"
+		>
+		</el-pagination>
 
-    <import ref="importRef" @refresh-data-list="getDataList"></import>
-    <preview ref="previewRef" @refresh-data-list="getDataList"></preview>
-    <edit ref="editRef" @refresh-data-list="getDataList"></edit>
-    <generator ref="generatorRef" @refresh-data-list="getDataList"></generator>
-  </el-card>
+		<import ref="importRef" @refresh-data-list="getDataList"></import>
+		<preview ref="previewRef" @refresh-data-list="getDataList"></preview>
+		<edit ref="editRef" @refresh-data-list="getDataList"></edit>
+		<generator ref="generatorRef" @refresh-data-list="getDataList"></generator>
+	</el-card>
 </template>
 
 <script setup lang="ts">
-import {reactive, ref} from 'vue'
-import {IHooksOptions} from '@/hooks/interface'
-import {useCrud} from '@/hooks'
+import { reactive, ref } from 'vue'
+import { IHooksOptions } from '@/hooks/interface'
+import { useCrud } from '@/hooks'
 import Import from './import.vue'
 import Edit from './edit.vue'
 import Preview from './preview.vue'
 import Generator from './generator.vue'
-import {useTableSyncApi} from '@/api/table'
-import {ElMessage, ElMessageBox} from 'element-plus'
-import {useDownloadApi} from '@/api/generator'
+import { useTableSyncApi } from '@/api/table'
+import { ElMessage, ElMessageBox } from 'element-plus'
+import { useDownloadApi } from '@/api/generator'
 
 const state: IHooksOptions = reactive({
-  dataListUrl: '/gen/table/page',
-  deleteUrl: '/gen/table',
-  queryForm: {
-    tableName: ''
-  }
+	dataListUrl: '/gen/table/page',
+	deleteUrl: '/gen/table',
+	queryForm: {
+		tableName: ''
+	}
 })
 
 const importRef = ref()
@@ -77,44 +76,43 @@ const previewRef = ref()
 const generatorRef = ref()
 
 const importHandle = (id?: number) => {
-  importRef.value.init(id)
+	importRef.value.init(id)
 }
 
 const editHandle = (id?: number) => {
-  editRef.value.init(id)
+	editRef.value.init(id)
 }
 const previewHandle = (tableId?: number) => {
-  previewRef.value.init(tableId);
-};
+	previewRef.value.init(tableId)
+}
 const generatorHandle = (id?: number) => {
-  generatorRef.value.init(id)
+	generatorRef.value.init(id)
 }
 
 const downloadBatchHandle = () => {
-  const tableIds = state.dataListSelections ? state.dataListSelections : []
+	const tableIds = state.dataListSelections ? state.dataListSelections : []
 
-  if (tableIds.length === 0) {
-    ElMessage.warning('请选择生成代码的表')
-    return
-  }
+	if (tableIds.length === 0) {
+		ElMessage.warning('请选择生成代码的表')
+		return
+	}
 
-  useDownloadApi(tableIds)
+	useDownloadApi(tableIds)
 }
 
 const syncHandle = (row: any) => {
-  ElMessageBox.confirm(`确定同步数据表${row.tableName}吗?`, '提示', {
-    confirmButtonText: '确定',
-    cancelButtonText: '取消',
-    type: 'warning'
-  })
-      .then(() => {
-        useTableSyncApi(row.id).then(() => {
-          ElMessage.success('同步成功')
-        })
-      })
-      .catch(() => {
-      })
+	ElMessageBox.confirm(`确定同步数据表${row.tableName}吗?`, '提示', {
+		confirmButtonText: '确定',
+		cancelButtonText: '取消',
+		type: 'warning'
+	})
+		.then(() => {
+			useTableSyncApi(row.id).then(() => {
+				ElMessage.success('同步成功')
+			})
+		})
+		.catch(() => {})
 }
 
-const {getDataList, selectionChangeHandle, sizeChangeHandle, currentChangeHandle, deleteBatchHandle} = useCrud(state)
+const { getDataList, selectionChangeHandle, sizeChangeHandle, currentChangeHandle, deleteBatchHandle } = useCrud(state)
 </script>
--- maku-generator-ui/src/views/generator/preview.vue
+++ maku-generator-ui/src/views/generator/preview.vue
@@ -1,44 +1,38 @@
 <template>
-  <!-- 预览界面 -->
-  <el-dialog v-model="preview.open" draggable :close-on-click-modal="false" :title="preview.title" append-to-body>
-    <el-tabs v-model="preview.activeName">
-      <el-tab-pane
-          v-for="(item, key) in preview.data"
-          :label="item.fileName"
-          :name="key"
-          :key="key"
-      >
-        <code-m v-model="item.content" height="610"></code-m>
-      </el-tab-pane>
-    </el-tabs>
-  </el-dialog>
+	<!-- 预览界面 -->
+	<el-dialog v-model="preview.open" draggable :close-on-click-modal="false" :title="preview.title" append-to-body>
+		<el-tabs v-model="preview.activeName">
+			<el-tab-pane v-for="(item, key) in preview.data" :key="key" :label="item.fileName" :name="key">
+				<code-m v-model="item.content" height="610"></code-m>
+			</el-tab-pane>
+		</el-tabs>
+	</el-dialog>
 </template>
 
 <script setup lang="ts">
-import {reactive, ref} from 'vue'
-import {ElLoading} from 'element-plus'
-import {usePreviewApi} from "@/api/generator";
-import CodeM from "@/components/codemirror/CodeMirror.vue";
+import { reactive, ref } from 'vue'
+import { ElLoading } from 'element-plus'
+import { usePreviewApi } from '@/api/generator'
+import CodeM from '@/components/codemirror/CodeMirror.vue'
 
 const preview = reactive({
-  open: false,
-  title: "代码预览",
-  data: {},
-  activeName: ref(0)
-});
+	open: false,
+	title: '代码预览',
+	data: {},
+	activeName: ref(0)
+})
 const init = async (tableId?: number) => {
-  const loadingInstance = ElLoading.service({fullscreen: true});
-  try {
-    let resData = await usePreviewApi(tableId);
-    preview.activeName = 0;
-    preview.data = resData.data;
-    preview.open = true;
-  } finally {
-    loadingInstance.close();
-  }
-
+	const loadingInstance = ElLoading.service({ fullscreen: true })
+	try {
+		let resData = await usePreviewApi(tableId)
+		preview.activeName = 0
+		preview.data = resData.data
+		preview.open = true
+	} finally {
+		loadingInstance.close()
+	}
 }
 defineExpose({
-  init
+	init
 })
 </script>
